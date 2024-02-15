db5863f6c13034ac0352bdab7f25e58d33bdc067
Den <Den@Den.com>
2024-02-15 12:02:39 +0300

    initial commit

---
+++ venv_maker.cmd
@@ -0,0 +1,8 @@
+REM test
+
+set python_path=C:\Program Files\Python312
+set venv_path=C:\Users\ username \ path_to \ venv
+set venv_name=my_venv_name
+
+"%python_path%\python" -m venv %venv_path%\%venv_name%
+pause
